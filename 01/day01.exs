b612330b086bf8b77477d80c9c46b1423a12f4d9
Jochum Börger <jborger@xebia.com>
2018-12-01 15:11:02 +0100

    Use MapSet instead of Map

--- 01/day01.exs
+++ 01/day01.exs
@@ -1,22 +1,18 @@
 defmodule Day01 do
-  def cycle(list), do: cycle(list, list, 0, %{0 => true})
+  def cycle(list), do: cycle(list, list, 0, MapSet.new([0]))
   def cycle([], orig, acc, seen), do: cycle(orig, orig, acc, seen)
   def cycle([h|t], orig, acc, seen) do
     next = acc + h
-    case seen[next] do
-      nil -> cycle(t, orig, next, Map.put(seen, next, true))
-      _ -> next
+    case MapSet.member?(seen, next) do 
+      true -> next
+      _    -> cycle(t, orig, next, MapSet.put(seen, next))
     end
   end
 end
 
-{:ok, file} = File.open("input", [:read])
-list = file
-  |> IO.stream(:line)
-  |> Enum.map(&String.trim_trailing/1)
+list = File.stream!("input")
+  |> Stream.map(&String.trim_trailing/1)
   |> Enum.map(&String.to_integer/1)
-File.close(file)
 
 list |> Enum.sum |> IO.puts
 list |> Day01.cycle |> IO.puts
-
