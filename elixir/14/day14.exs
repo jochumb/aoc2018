dc7d3605d813226d1d85a75153deccdf0290420f
Jochum Börger <jborger@xebia.com>
2018-12-14 23:55:41 +0100

    Cleanup 14

--- elixir/14/day14.exs
+++ elixir/14/day14.exs
@@ -4,7 +4,7 @@ defmodule Day14 do
   end
 
   def recipes_before(recipes) do
-    iterate2(%{0 => 3, 1 => 7}, {0,1}, recipes, String.length(recipes))
+    iterate2(%{0 => 3, 1 => 7}, {0,1}, :binary.compile_pattern(recipes), String.length(recipes))
   end
 
   def iterate(map, pos, recipes) do
@@ -17,7 +17,7 @@ defmodule Day14 do
   def iterate2(map, pos, recipes, length) do
     size = map_size(map)
     case recipes_at_end(map, size, recipes, length) do
-      {:ok,count} -> count
+      {:ok, count} -> count
       :cont -> next(map, pos, size) |> fn {nm, np} -> iterate2(nm, np, recipes, length) end.()
     end
   end
@@ -34,26 +34,26 @@ defmodule Day14 do
   end
 
   def next(map, {a,b}, size) do
-    a_val = Map.get(map, a)
-    b_val = Map.get(map, b)
-    recipe = (a_val + b_val) |> Integer.digits
-    new_map = case recipe do
-      [s|[]] -> Map.put(map, size, s)
-      [s,t|_] -> Map.put(map, size, s) |> Map.put(size+1, t)
+    %{^a => a_val, ^b => b_val} = map
+    {new_map, c} = case a_val + b_val do
+      n when n > 9 -> {Map.put(map, size, 1) |> Map.put(size+1, n-10),2}
+      n -> {Map.put(map, size, n),1}
     end
-    new_pos = {rem(a + a_val + 1, size + Enum.count(recipe)), rem(b + b_val + 1, size + Enum.count(recipe))}
+    new_pos = {rem(a + a_val + 1, size + c), rem(b + b_val + 1, size + c)}
     {new_map, new_pos}
   end
 
   def recipes_at_end(_, size, _, length) when size <= length, do: :cont
   def recipes_at_end(map, size, recipes, length) do
-    str = size-(length+1)..size-1 
+    split = size-(length+1)..size-1 
       |> Enum.map(fn n -> Map.get(map, n) end) 
       |> Enum.map(&Integer.to_string/1) 
       |> Enum.join
-    case String.contains?(str, recipes) do
-      true -> if String.starts_with?(str, recipes), do: {:ok, size-(length+1)}, else: {:ok, size-length}
-      false -> :cont
+      |> String.split(recipes)
+    case split do
+      ["",_|[]] -> {:ok, size-length}
+      [_,""|[]] -> {:ok, size-(length+1)}
+      _         -> :cont
     end
   end
 end
